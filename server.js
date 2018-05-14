a7dffe30c1a84a87714268b5956b69b2778bce3a
BrandoMutiz <brandonsteven18_@hotmail.com>
2018-05-14 15:37:21 -0500

    first commit t

--- server.js
+++ server.js
@@ -102,7 +102,50 @@ let mundialitoscore= [
 														 
 														 
 														 
-						}	
+						},	
+						{			
+
+			id : 1, 
+			nom_preg:'¿En qué país se celebró la  primera edición de la Copa del Mundo de fútbol?', 
+			respuestas: [
+			{
+				id: 1,
+				titulo_resp:'Francia',
+				estado:'0'
+			},
+			{
+				id: 2 ,
+				titulo_resp:'Inglaterra',
+				estado:'0'
+			},
+			{
+				id: 3 ,
+				titulo_resp:'Uruguay',
+				estado:'1'
+			},
+			{
+				id: 4 ,
+			titulo_resp:'Holanda',
+			estado:'0'
+			}
+																																
+		]
+
+														}
+
+
+
+														
+														 
+									
+																												
+									
+									
+									                     ]
+														 
+														 
+														 
+						}
 						
                      ];
 
